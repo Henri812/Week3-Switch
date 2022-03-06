0ab21aaadeb20656867491365a4384871005da4b
Henri Heinmäe <henriheinmae@gmail.com>
2022-03-06 22:29:17 +0200

    add new projects

---
+++ GradingSwitch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradingSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib, mis hinde kasutaja sai
+            //kui kasutaja sai 'A', konsool kuvab "Suurepärane";
+            //'B', konsool kuvab "Väga hea!"
+            //'C', konsool kuvab "Hea!";
+            //'D', konsool kuvab "Rahuldav";
+            //'E', konsool kuvab "Kasin";
+            //'F', konsool kuvab "Puudulik".
+            //kui kasutaja sistab midagi muud, siis konsool kuvab "vale väärtus"
+
+            //Kasutame SWITCH
+        }
+    }
+}
---
+++ PersonalityTestSwitch/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalityTestSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada tema lemmik värv;
+            //kui kasutaja sisestab "punane", konsool kuvab "oled romantiline";
+            //kui kasutaja sisestab "sinine", konsool kuvab "oled töökas";
+            //kui kasutaja sisestab "roheline", konsool kuvab "oled looduse sõber";
+            //Kui kasutaja sisestab midagi muud, konsool kuvab "oled {userColor} ükssarvik";
+
+            Console.WriteLine("Sisesta oma lemmikvärv");
+            string userColor = Console.ReadLine().ToLower();
+
+            switch (userColor)
+            {
+                case "punane": //if(userColor == "punane")
+                    Console.WriteLine("Oled romantiline");
+                    break;
+                case "sinine":
+                    Console.WriteLine("Oled töökas.");
+                    break;
+                case "roheline":
+                    Console.WriteLine("Oled looduse sõber.");
+                    break;
+                default:
+                    Console.WriteLine("$Oled {userColor} ükssarvik.");
+                    break;
+
+            }
+
+
+            Console.WriteLine("Kena päeva!");
+
+
+        }
+    }
+}
